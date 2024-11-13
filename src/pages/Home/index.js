23ac14c3c1e6652178bf3063d7e06ae31182a01f
l-fraga2811 <lucasbelter2805@gmail.com>
2024-11-12 21:04:06 -0300

    Finalizando webapp com consumo de API do GitHub

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,32 @@
 import { Header } from "../../components/Header";
-import background from "../../assets/home-background.png";
 import "./styles.css";
 import ItemList from "../../components/ItemList";
+import { useState } from "react";
+import background from "../../assets/home-background.png";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    if (newUser.name) {
+      const { avatar_url, name, bio, login } = newUser;
+      setCurrentUser({ avatar_url, name, bio });
+    }
+
+    const repoData = await fetch(`https://api.github.com/users/${user}/repos`);
+    const newRepos = await repoData.json();
+    if (newRepos.length > 0) {
+      setRepos(newRepos);
+    }
+  };
+
+  /*-------------------------------------------------------------------------------------------------------- */
+
   return (
     <div className="App">
       <Header />
@@ -15,31 +38,43 @@ function App() {
         />
         <div className="info">
           <div>
-            <input name="usuario" placeholder="@username" />
-            <button>Buscar</button>
-          </div>
-          <div className="perfil">
-            <img
-              src="https://avatars.githubusercontent.com/u/161073722?v=4"
-              className="profile"
-              alt="profilePicture"
+            <input
+              name="usuario"
+              value={user}
+              onChange={(event) => setUser(event.target.value)}
+              placeholder="@username"
             />
+            <button onClick={handleGetData}>Buscar</button>
+          </div>
+          {currentUser?.name ? (
+            <>
+              <div className="perfil">
+                <img
+                  src= {currentUser.avatar_url}
+                  className="profile"
+                  alt="profilePicture"
+                />
+                <div>
+                  <h3>{currentUser.name}</h3>
+                  <span>{currentUser.login}</span>
+                  <p> {currentUser.bio}</p>
+                </div>
+              </div>
+              <hr />
+            </>
+          ) : null}
+          {repos?.length ? (
+            
             <div>
-              <h3>Lucas Fraga</h3>
-              <span>
-                Instagram:{" "}
-                <a href="https://www.instagram.com/_lfraga/">@_lfraga</a>
-              </span>
-              <p>aaaaaaaaaaaaaaaaa</p>
+              <h4 className="repositorio">Repositórios</h4>
+
+              {repos.map((repo) => (
+              
+              <ItemList title={repo.name} description={repo.description}/>
+              ))}
+              
             </div>
-          </div>
-          <hr />
-          <div>
-            <h4 className="repositorio">Repositórios</h4>
-            <ItemList title = "Teste 1" description="teste de descrição"/>
-            <ItemList title = "Teste 1" description="teste de descrição"/>
-            <ItemList title = "Teste 1" description="teste de descrição"/>
-          </div>
+          ) : null}
         </div>
       </div>
     </div>
